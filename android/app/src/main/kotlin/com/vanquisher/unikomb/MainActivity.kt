790aa940fc820daf839dbdab35aa055efb82565f
GAGANJOT SINGH <121406281+skylar6194@users.noreply.github.com>
2023-07-31 22:22:49 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/vanquisher/unikomb/MainActivity.kt
+++ android/app/src/main/kotlin/com/vanquisher/unikomb/MainActivity.kt
@@ -4,3 +4,27 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
+//Added by skyular 6194
+function onReady(assets) {
+  const picker = NativeUI.picker;
+
+  const index = 0;
+  const selection = 0;
+
+  const configuration = {
+    selectedIndex: index,
+
+    items: [
+      { image_texture: texture0 },
+      { image_texture: texture1 },
+      { image_texture: texture2 },
+    ],
+  };
+
+  picker.configure(configuration);
+  picker.visible = true;
+
+  picker.selectedIndex.monitor().subscribe(function (index) {
+    Patches.inputs.setScalar("selection", index.newValue);
+  });
+}
